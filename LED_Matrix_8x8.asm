7288b5fa400bfd8d870ba2a5066820499510a4ce
Tarek Khalifa <tarek.khalifa16@gmail.com>
2018-08-20 19:55:51 +0200

    Display 'K' character with Multiplexing (1ms).

--- LED_Matrix_8x8.asm
+++ LED_Matrix_8x8.asm
@@ -12,15 +12,94 @@ _main:
 	CLRF        PORTD+0 
 ;LED_Matrix_8x8.c,7 :: 		while(1){
 L_main0:
-;LED_Matrix_8x8.c,8 :: 		PORTC = 0xFF;
-	MOVLW       255
+;LED_Matrix_8x8.c,8 :: 		PORTC = 0b11111110;
+	MOVLW       254
 	MOVWF       PORTC+0 
-;LED_Matrix_8x8.c,9 :: 		PORTD = 0xFF;
+;LED_Matrix_8x8.c,9 :: 		PORTD = 0b11111111;
 	MOVLW       255
 	MOVWF       PORTD+0 
-;LED_Matrix_8x8.c,12 :: 		}
+;LED_Matrix_8x8.c,10 :: 		delay_ms(1);
+	MOVLW       6
+	MOVWF       R12, 0
+	MOVLW       48
+	MOVWF       R13, 0
+L_main2:
+	DECFSZ      R13, 1, 1
+	BRA         L_main2
+	DECFSZ      R12, 1, 1
+	BRA         L_main2
+	NOP
+;LED_Matrix_8x8.c,12 :: 		PORTC = 0b11111101;
+	MOVLW       253
+	MOVWF       PORTC+0 
+;LED_Matrix_8x8.c,13 :: 		PORTD = 0b00011000;
+	MOVLW       24
+	MOVWF       PORTD+0 
+;LED_Matrix_8x8.c,14 :: 		delay_ms(1);
+	MOVLW       6
+	MOVWF       R12, 0
+	MOVLW       48
+	MOVWF       R13, 0
+L_main3:
+	DECFSZ      R13, 1, 1
+	BRA         L_main3
+	DECFSZ      R12, 1, 1
+	BRA         L_main3
+	NOP
+;LED_Matrix_8x8.c,16 :: 		PORTC = 0b11111011;
+	MOVLW       251
+	MOVWF       PORTC+0 
+;LED_Matrix_8x8.c,17 :: 		PORTD = 0b00100100;
+	MOVLW       36
+	MOVWF       PORTD+0 
+;LED_Matrix_8x8.c,18 :: 		delay_ms(1);
+	MOVLW       6
+	MOVWF       R12, 0
+	MOVLW       48
+	MOVWF       R13, 0
+L_main4:
+	DECFSZ      R13, 1, 1
+	BRA         L_main4
+	DECFSZ      R12, 1, 1
+	BRA         L_main4
+	NOP
+;LED_Matrix_8x8.c,20 :: 		PORTC = 0b11110111;
+	MOVLW       247
+	MOVWF       PORTC+0 
+;LED_Matrix_8x8.c,21 :: 		PORTD = 0b01000010;
+	MOVLW       66
+	MOVWF       PORTD+0 
+;LED_Matrix_8x8.c,22 :: 		delay_ms(1);
+	MOVLW       6
+	MOVWF       R12, 0
+	MOVLW       48
+	MOVWF       R13, 0
+L_main5:
+	DECFSZ      R13, 1, 1
+	BRA         L_main5
+	DECFSZ      R12, 1, 1
+	BRA         L_main5
+	NOP
+;LED_Matrix_8x8.c,24 :: 		PORTC = 0b11101111;
+	MOVLW       239
+	MOVWF       PORTC+0 
+;LED_Matrix_8x8.c,25 :: 		PORTD = 0b10000001;
+	MOVLW       129
+	MOVWF       PORTD+0 
+;LED_Matrix_8x8.c,26 :: 		delay_ms(1);
+	MOVLW       6
+	MOVWF       R12, 0
+	MOVLW       48
+	MOVWF       R13, 0
+L_main6:
+	DECFSZ      R13, 1, 1
+	BRA         L_main6
+	DECFSZ      R12, 1, 1
+	BRA         L_main6
+	NOP
+;LED_Matrix_8x8.c,28 :: 		}
 	GOTO        L_main0
-;LED_Matrix_8x8.c,13 :: 		}
+;LED_Matrix_8x8.c,29 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
--- LED_Matrix_8x8.cp
+++ LED_Matrix_8x8.cp
@@ -6,9 +6,25 @@ void main() {
  PORTD = 0x00;
 
  while(1){
- PORTC = 0xFF;
- PORTD = 0xFF;
+ PORTC = 0b11111110;
+ PORTD = 0b11111111;
+ delay_ms(1);
 
+ PORTC = 0b11111101;
+ PORTD = 0b00011000;
+ delay_ms(1);
+
+ PORTC = 0b11111011;
+ PORTD = 0b00100100;
+ delay_ms(1);
+
+ PORTC = 0b11110111;
+ PORTD = 0b01000010;
+ delay_ms(1);
+
+ PORTC = 0b11101111;
+ PORTD = 0b10000001;
+ delay_ms(1);
 
  }
 }
